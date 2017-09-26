06972c47547dc16db28550a3a2ce1126b85d2849
Zack <Zackcolemanmusic@gmail.com>
2017-09-26 16:32:02 +0100

    Changed Arguments

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -3,18 +3,18 @@ return 10
 end
 
 
-def add(one, two)
-return (one + two)
+def add(first_number, second_number)
+return (first_number + second_number)
 end
 
-def subtract(ten, five)
-  return (10 - 5)
+def subtract(first_number, second_number)
+  return (first_number - second_number)
 end
 
-def multiply(four, two)
-  return (4 * 2)
+def multiply(first_number, second_number)
+  return (first_number * second_number)
 end
 
-def divide(ten, two)
-  return (10 / 2)
+def divide(first_number, second_number)
+  return (first_number / second_number)
 end
