3cb0a6cd7601a83f2a45ca057a8b6fe05a7f72b2
Zack <Zackcolemanmusic@gmail.com>
2017-09-26 17:01:23 +0100

    Passed Month Full Name.

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -30,3 +30,18 @@ end
 def add_string_as_number(string1 , string2)
   return string1.to_i + string2.to_i
 end
+
+def number_to_full_month_name(number)
+  case number
+
+  when 1
+    return "January"
+
+  when 3
+    return "March"
+
+  when 9
+    return "September"
+
+   end
+end
