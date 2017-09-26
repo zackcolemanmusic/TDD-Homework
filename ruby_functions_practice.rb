0d040290dde89ee0c1aa28884da0f551e70d5b01
Zack <Zackcolemanmusic@gmail.com>
2017-09-26 17:07:55 +0100

    Passed short month

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -45,3 +45,19 @@ def number_to_full_month_name(number)
 
    end
 end
+
+def number_to_short_month_name(number)
+
+  case number
+
+  when 1
+    return "Jan"
+
+  when 3
+    return "Mar"
+
+  when 9
+    return "Sep"
+
+   end
+ end
